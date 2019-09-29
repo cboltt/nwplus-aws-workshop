a938117c9ba70b19c7c310ef2e5b9866c4fe359b
Chris Bolton <chris.bolton97@gmail.com>
2019-09-29 15:18:02 -0700

    removes boilerplate

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -15,6 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
