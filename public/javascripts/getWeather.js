82fae530339068a696e826e00b242304879c180d
Chris Bolton <chris.bolton97@gmail.com>
2019-09-30 16:12:32 -0700

    refactors getWeather script into functions

--- public/javascripts/getWeather.js
+++ public/javascripts/getWeather.js
@@ -1,31 +1,57 @@
-function outputResponse(className, text) {
+// Outputs reponse to page with given class name and message
+// if output element exists
+function outputResponse(className, message) {
   var outputElement = document.querySelector("#response-output");
 
+  if (!outputElement) {
+    return;
+  }
+
   outputElement.classList = className;
-  outputElement.innerHTML = text;
+  outputElement.innerHTML = message;
   document.activeElement.blur();
 }
 
+// Handles the response of the weather API call
+function handleFormResponse(data) {
+  if (data.weather) {
+    outputResponse("weather", data.weather[0].description);
+  } else {
+    outputResponse("error", data.message);
+  }
+}
+
+// Returns the value of the city name input if it exists
+// (returns empty string otherwise)
+function getCityInputValue() {
+  var cityInputElement = document.querySelector("input[name='city']");
+
+  if (!cityInputElement) {
+    return "";
+  }
+
+  return cityInputElement.value;
+}
+
+// Handles form submission by making API call to weather endpoint
+// with given city name
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  fetch("/weather/" + getCityInputValue(), {
+    method: "get",
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(r => r.json())
+    .then(data => handleFormResponse(data));
+}
+
+// Creates event listener for form submission once the page
+// has loaded and form element exists
 document.addEventListener("DOMContentLoaded", function() {
-  document
-    .querySelector("#weather-form")
-    .addEventListener("submit", function(event) {
-      event.preventDefault();
+  var weatherFormElement = document.querySelector("#weather-form");
 
-      var city = document.querySelector("input[name='city']").value;
-      var options = {
-        method: "get",
-        headers: { "Content-Type": "application/json" }
-      };
-
-      fetch("/weather/" + city, options)
-        .then(r => r.json())
-        .then(data => {
-          if (data.weather) {
-            outputResponse("weather", data.weather[0].description);
-          } else {
-            outputResponse("error", data.message);
-          }
-        });
-    });
+  if (weatherFormElement) {
+    weatherFormElement.addEventListener("submit", handleFormSubmit);
+  }
 });
